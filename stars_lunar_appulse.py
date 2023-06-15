ac04bc171ac23ddfd60e003fbf6d4041054930ea
hkspm-yp <110584410+hkspm-yp@users.noreply.github.com>
2023-06-15 23:01:37 +0800

    Update stars_lunar_appulse.py

--- stars_lunar_appulse.py
+++ stars_lunar_appulse.py
@@ -1,17 +1,19 @@
-from skyfield.api import Star, load
-# planets = load('de440.bsp')
-# earth = planets['earth']
-# ts = load.timescale()
-# t = ts.now()
-# astrometric = earth.at(t).observe(barnard)
-# ra, dec, distance = astrometric.radec()
-# print(ra)
-# print(dec)
+ # This script considers speical stars lunar appulse events which fulfill the following:
+ # (1) the separation is less that 1 degree;
+ # (2) the moon is above the horizon; 
+ # (3) the Sun is 15 degrees below the horizon.
 
+
+from skyfield.api import Star, load
 from skyfield.framelib import ecliptic_frame
 from skyfield.searchlib import find_minima
 from main import *
 from pytz import timezone
+import numpy as np
+
+Earth_radius=6378.1366
+k_IAU=0.2725076
+moon_radius_km=Earth_radius*k_IAU
 
 name_list_chi=['角宿一、月球最小角距', '軒轅十四、月球最小角距', '心宿二、月球最小角距', '畢宿五、月球最小角距', '北河三、月球最小角距']
 name_list_eng=['Spica-Moon Appulse',
@@ -41,31 +43,39 @@ for i in range(5):
     separation_times, separation = find_minima(t0, t1, separation_at)
 
     for t, separation_degrees in zip(separation_times, separation):
-        temp_list_stars_appulse=['0=event_Chi', '1=event_Eng','2=date(dd/mm/yyyy)','3=time(hh/mm)','4=remark','5=level']
-        temp_list_stars_appulse[2]=t.astimezone(HKT).date()
-        temp_list_stars_appulse[3]=(float(t.astimezone(HKT).time().hour)+float(t.astimezone(HKT).time().minute)/60+float(t.astimezone(HKT).time().second)/3600)/24
-        phase = almanac.moon_phase(eph, t)
-        # temp_list_stars_appulse[4]='角距 Angular separation: ' + str("%0.2f" % separation_degrees) +'°; ' + 'Phase degrees of the Moon is ' + str(int(phase.degrees))+'°'
-        temp_list_stars_appulse[4]='角距 Angular separation: ' + str("%0.2f" % separation_degrees) +'°'
-        temp_list_stars_appulse[1]=name_list_eng[i]
-        temp_list_stars_appulse[0]=name_list_chi[i]
-        if separation_degrees<0.25:
-            temp_list_stars_appulse[0]=name_list_chi[i]+'(掩)'
-            temp_list_stars_appulse[1]=name_list_eng[i]+'(Occultation)'
-        temp_list_stars_appulse[5]='?'
-        # if i ==0 or i == 5 or i == 6:
-        #     temp_list_stars_appulse[8]='3' 
-        # if i == 2 or i ==3 or i ==4: # Mars, jupiter and saturn
-        if phase.degrees > 330 or phase.degrees <30:
-            temp_list_stars_appulse[5]=3
-        elif phase.degrees > 300 or phase.degrees <60:
-            if separation_degrees < 1:
-                temp_list_stars_appulse[5]=1
-            else:
+        e = (earth + HKO).at(t)
+        m = e.observe(moon).apparent().altaz()[0].degrees
+        s = e.observe(sun).apparent().altaz()[0].degrees
+        moon_distance=e.observe(moon).apparent().distance().km
+        moon_angular_radius_degrees=np.arctan(moon_radius_km/moon_distance)*(180/np.pi)
+        if separation_degrees<1 and m >0 and s <-15: # change the conditions by the values here.
+        
+            temp_list_stars_appulse=['0=event_Chi', '1=event_Eng','2=date(dd/mm/yyyy)','3=time(hh/mm)','4=remark','5=level']
+            temp_list_stars_appulse[2]=t.astimezone(HKT).date()
+            temp_list_stars_appulse[3]=(float(t.astimezone(HKT).time().hour)+float(t.astimezone(HKT).time().minute)/60+float(t.astimezone(HKT).time().second)/3600)/24
+            phase = almanac.moon_phase(eph, t)
+            # temp_list_stars_appulse[4]='角距 Angular separation: ' + str("%0.2f" % separation_degrees) +'°; ' + 'Phase degrees of the Moon is ' + str(int(phase.degrees))+'°'
+            temp_list_stars_appulse[4]='角距 Angular separation: ' + str("%0.2f" % separation_degrees) +'°'
+            temp_list_stars_appulse[1]=name_list_eng[i]
+            temp_list_stars_appulse[0]=name_list_chi[i]
+            if separation_degrees < moon_angular_radius_degrees:
+                temp_list_stars_appulse[0]=name_list_chi[i]+'(掩)'
+                temp_list_stars_appulse[1]=name_list_eng[i]+'(Occultation)'
+                                
+            temp_list_stars_appulse[5]='?'
+            # if i ==0 or i == 5 or i == 6:
+            #     temp_list_stars_appulse[8]='3' 
+            # if i == 2 or i ==3 or i ==4: # Mars, jupiter and saturn
+            if phase.degrees > 330 or phase.degrees <30:
+                temp_list_stars_appulse[5]=3
+            elif phase.degrees > 300 or phase.degrees <60:
+                if separation_degrees < 1:
+                    temp_list_stars_appulse[5]=1
+                else:
+                    temp_list_stars_appulse[5]=2
                 temp_list_stars_appulse[5]=2
-            temp_list_stars_appulse[5]=2
-        else:
-            temp_list_stars_appulse[5]=1
-        print(temp_list_stars_appulse)
-        phase = almanac.moon_phase(eph, t)
-        list_stars_appulse.append(temp_list_stars_appulse)      
+            else:
+                temp_list_stars_appulse[5]=1
+            print(temp_list_stars_appulse)
+            phase = almanac.moon_phase(eph, t)
+            list_stars_appulse.append(temp_list_stars_appulse)      
