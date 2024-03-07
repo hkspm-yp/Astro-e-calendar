58bda2b979d7c6174bb68b1e6b98da1f93b55533
hkspm-yp <110584410+hkspm-yp@users.noreply.github.com>
2024-03-07 10:56:22 +0800

    Update and rename opposition.py to oppositions_conjunctions.py

--- oppositions_conjunctions.py
+++ oppositions_conjunctions.py
@@ -1,7 +1,7 @@
 from skyfield import api
 from skyfield import almanac
 from pytz import timezone
-from main import t0, t1, eph, HKT
+from main import *
 
 target=['MERCURY BARYCENTER', 
         'VENUS BARYCENTER', 
@@ -10,19 +10,16 @@ target=['MERCURY BARYCENTER',
         'SATURN BARYCENTER', 
         'URANUS BARYCENTER', 
         'NEPTUNE BARYCENTER', 
-        'PLUTO BARYCENTER', 
-        'SUN', 
-        'MERCURY', 
-        'VENUS', 
+        # 'PLUTO BARYCENTER', 
         ]
 Opposition_eng=['Superior Conjunction of Mercury', 
-        'Superior Conjunctionof Venus', 
+        'Superior Conjunction of Venus', 
         'Opposition of Mars', 
         'Opposition of Jupiter', 
         'Opposition of Saturn', 
         'Opposition of Uranus', 
         'Opposition of Neptune', 
-        'Opposition of Pluto', 
+        # 'Opposition of Pluto', 
         ]
 Opposition_chi=['水星上合', 
         '金星上合', 
@@ -31,7 +28,7 @@ Opposition_chi=['水星上合',
         '土星衝', 
         '天王星衝', 
         '海王星衝', 
-        '冥王星衝', 
+        # '冥王星衝', 
         ]
 Conjunction_eng=['Inferior Conjunction of Mercury', 
         'Inferior Conjunction of Venus', 
@@ -40,7 +37,7 @@ Conjunction_eng=['Inferior Conjunction of Mercury',
         'Conjunction of Saturn', 
         'Conjunction of Uranus', 
         'Conjunction of Neptune', 
-        'Conjunction of Pluto', 
+        # 'Conjunction of Pluto', 
         ]
 Conjunction_chi=['水星下合', 
         '金星下合', 
@@ -49,17 +46,19 @@ Conjunction_chi=['水星下合',
         '土星上合', 
         '天王星上合', 
         '海王星上合', 
-        '冥王星上合', 
+        # '冥王星上合', 
         ]
 list_oppositions_conjunctions=[]
-for i in range(8):
+for i in range(7): # range(8) if Pluto is considered.
     f = almanac.oppositions_conjunctions(eph, eph[target[i]])
+
     t, y = almanac.find_discrete(t0, t1, f)
 
     for yi, ti in zip(y, t):
         temp_list_oppositions_conjunctions=['0=event_Chi', '1=event_Eng','2=date(dd/mm/yyyy)','3=time(hh/mm)','4=remark','5=level']
         temp_list_oppositions_conjunctions[2]=ti.astimezone(HKT).date()
         temp_list_oppositions_conjunctions[3]=(float(ti.astimezone(HKT).time().hour)+float(ti.astimezone(HKT).time().minute)/60+float(ti.astimezone(HKT).time().second)/3600)/24
+        # temp_list_oppositions_conjunctions[3]=t.utc_datetime()
         temp_list_oppositions_conjunctions[4]=''       
         temp_list_oppositions_conjunctions[5]='?'
         if i == 0 or i ==1 or i == 6 or i ==7:
